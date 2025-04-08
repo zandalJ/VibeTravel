e33613a086da7010d8cafc9a33adac9269559174
“mkczarkowski” <marcinczarkowski@onet.eu>
2025-04-08 21:09:14 +0200

    fix: add node adapter (for building in server mode)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
